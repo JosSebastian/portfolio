a4618de7700b29938acda9bf12dc519c8179941b
sebas <sebastian.jos2222@gmail.com>
2023-09-24 17:57:22 -0500

    initial section one implementation

---
+++ components/section/one.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <div class="flex flex-col text-3xl md:text-4xl lg:text-5xl font-bold">
+      <span>Hi.</span>
+      <span>I'm Jos Sebastian,</span>
+      <span>A Computer Engineer.</span>
+    </div>
+    <span class="text-xl font-semibold">
+      Student at Texas A&M University.
+    </span>
+  </div>
+</template>
+
+<style scoped></style>
