25fb0ca1e304027ee71f5b86b9ea9b9ed6191b68
sebas <sebastian.jos2222@gmail.com>
2023-09-24 18:54:37 -0500

    section one layout changes

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <script lang="ts" setup></script>
 
 <template>
-  <div class="w-full h-full flex flex-col lg:flex-row lg:gap-3">
-    <div class="lg:w-1/2 max-h-screen lg:sticky top-0"></div>
-    <div class="lg:w-1/2 min-h-screen"></div>
+  <div class="w-full h-full flex flex-col lg:flex-row">
+    <div
+      class="lg:w-1/2 max-h-screen p-5 md:p-7.5 lg:p-10 lg:sticky top-0 flex flex-col justify-between lg:items-center">
+      <SectionOne />
+    </div>
+    <div class="lg:w-1/2 min-h-screen p-5 md:p-7.5 lg:p-10"></div>
   </div>
 </template>
 
