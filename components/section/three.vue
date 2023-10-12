1412d4959922100a5b32273224e1f57b8b2a5f21
sebas <sebastian.jos2222@gmail.com>
2023-10-12 08:52:17 -0500

    layout change and about page text implementation

--- components/section/three.vue
+++ components/section/three.vue
@@ -2,7 +2,27 @@
 
 <template>
   <PrimitiveSection title="ABOUT">
-    <div class="px-5 py-0 text-lg"></div>
+    <div class="flex flex-col px-5 py-0 text-lg">
+      <span>
+        Since my initial encounter with computers during my early adolescence,
+        my fascination with the field has continuously deepened, solidifying my
+        belief that it is my life's true passion. As soon as I had the
+        opportunity, I embarked on a gradual exploration of both software and
+        hardware engineering, eventually discovering my profound affection for
+        <span class="font-bold hover:text-blue-100"> Computer Graphics </span>.
+        This discipline genuinely captivates me with its unique appeal,
+        compelling me to pursue a comprehensive understanding of its
+        intricacies. Along my journey, I also ventured into
+        <span class="font-bold"> Computer Architecture </span>as well as
+        <span class="font-bold"> Hardware and Software Engineering </span>.
+      </span>
+      <br />
+      <span>
+        To expedite my exploration of these interests, I proactively initiated
+        several personal projects, each contributing valuable insights to my
+        chosen path.
+      </span>
+    </div>
   </PrimitiveSection>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -3,12 +3,12 @@
 <template>
   <div class="flex flex-col w-full h-full lg:flex-row">
     <div
-      class="top-0 flex flex-col justify-between max-h-screen p-5 lg:w-1/2 lg:p-10 lg:sticky lg:items-center">
+      class="top-0 flex flex-col justify-between max-h-screen p-5 lg:w-1/2 lg:px-9 lg:py-24 lg:sticky lg:items-center">
       <SectionOne />
       <SectionTwo />
     </div>
     <div
-      class="top-0 flex flex-col min-h-screen justify-normal lg:w-1/2 lg:p-10">
+      class="top-0 flex flex-col items-center min-h-screen lg:w-1/2 lg:px-18 lg:py-24 lg:gap-12">
       <SectionThree />
       <SectionFour />
       <SectionFive />
