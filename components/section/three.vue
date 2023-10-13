d0c2e845ce69d76def2d5c4879f363dc80827aa1
sebas <sebastian.jos2222@gmail.com>
2023-10-13 18:53:58 -0500

    about page text change

--- components/section/three.vue
+++ components/section/three.vue
@@ -8,13 +8,11 @@
         my fascination with the field has continuously deepened, solidifying my
         belief that it is my life's true passion. As soon as I had the
         opportunity, I embarked on a gradual exploration of both software and
-        hardware engineering, eventually discovering my profound affection for
-        <span class="font-bold"> Computer Graphics </span>. This discipline
-        genuinely captivates me with its unique appeal, compelling me to pursue
-        a comprehensive understanding of its intricacies. Along my journey, I
-        also ventured into
-        <span class="font-bold"> Computer Architecture </span>as well as
-        <span class="font-bold"> Hardware and Software Engineering </span>.
+        hardware engineering, eventually discovering my affection for Computer
+        Graphics. I'm truly captivated by the allure of this field, which drives
+        me to seek a thorough comprehension of its complexities. Along my
+        journey, I also ventured into Computer Architecture as well as the other
+        various aspects of Hardware and Software Engineering.
       </span>
       <span>
         To expedite my exploration of these interests, I proactively initiated
